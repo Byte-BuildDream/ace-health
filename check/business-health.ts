4d817822a6d3579906c2a8cda9a075e7b541722f
zerokaze420 <erfu81856@gmail.com>
2025-09-22 19:35:58 +0800

    feat(workflow): 添加飞书集成测试工作流
    新增定时任务与CI流程，用于定期检查业务健康状态。
    该工作流每10分钟执行一次，确保及时发现并通知异常情况。
    
    - 添加 GitHub Actions 配置文件 `.github/workflows/check.yml`
    - 实现业务健康检查脚本 `check/business-health.ts`
    - 集成飞书通知机制，异常时自动发送警告卡片消息

---
+++ check/business-health.ts
@@ -0,0 +1,63 @@
+import axios from "axios"
+import { sendFeishuCard } from '../src/utils/sendCard';
+import { createNotifyCard } from '../src/cards/notify';
+interface resbody {
+    "status": string
+    "health_score": number,
+    "response_time_ms": number,
+    "failed_functions": null
+}
+
+
+
+
+export async function sendCardForBusinessHealth(res : string) {
+     const card = createNotifyCard({
+                title: '⚠️警告',
+                content: `前端出现异常错误: **${res}**\n时间 ${new Date().toLocaleString()} `,
+                atMobiles: ['<at id=all>'], // 替换为你想@的人手机号
+                color: 'red',
+    });
+
+    await sendFeishuCard(card);
+}
+
+
+export async function health(url: string): Promise<resbody> {
+
+    try {
+        const res = await axios.get(url);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            sendCardForBusinessHealth("获取数据失败");
+            console.error("Axios 请求失败:", error.message);
+            throw new Error(`请求失败: ${error.message}`);
+        }
+        sendCardForBusinessHealth("获取数据失败");
+        console.error("未知错误:", error);
+        throw error;
+    }
+}
+
+
+
+
+export async function main() {
+    try {
+        const result = await health('http://playace.cn/api/business-health');
+        console.log("获取到的数据:", result);
+        if (result.status === "healthy") {
+            console.log("业务健康状态正常");
+        } else {
+            sendCardForBusinessHealth("无法访问业务API");
+        
+        }
+    } catch (e) {
+        console.error("主函数错误:", e.message);
+    }
+
+
+}
+
+main();
